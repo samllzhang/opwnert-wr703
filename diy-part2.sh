2126a60658a791c15d93adbf1ee08ce63b75d49b
samllzhang <68906880+samllzhang@users.noreply.github.com>
2022-03-17 12:50:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+#修改密码为空（去掉#生效）
+#ZZZ="package/lean/default-settings/files/zzz-default-settings"
+#sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+
+#修改路由器编译版本名称（/#####内容修改成自己想要的名字）
+#sed -i "s/OpenWrt /######################## compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
